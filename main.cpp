74585400c0738f639007e4c65d29cd85e01a5ff4
ChanyoungKim <davidkjcy@gmail.com>
2023-03-03 16:39:58 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	int x{ 1 }, y{ 2 };
+
+	// 연산자의 기능
+	// = 왼쪽에다가 오른쪽을 더해서 결과를 돌려줌
+	x + y;
+
+	x == y;
+	
+	bool flag = true;
+	if (!flag)
+	{
+
+	}
+
+	// 연산자가 마치 함수같은 원리라고 생각할 수 있다.
+	/*
+	ex)
+	int Sum (int a, int b)
+	{
+		return a + b;
+	}
+	*/
+}
